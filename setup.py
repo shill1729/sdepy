483302146dfd7374d4e190e745c1c1aedb05276b
seanhill <seamhill93@gmail.com>
2023-11-13 14:06:38 -0500

    update from 1.14 to 1.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sdepy',
-    version='1.14',
+    version='1.15',
     packages=['sdepy'],
     url='https://github.com/shill1729/sdepy',
     requires=["numpy", "sympy", "matplotlib", "scipy"],
