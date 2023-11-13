fa04a1296ef7904de1b83025450aa195e334e803
seanhill <seamhill93@gmail.com>
2023-11-13 11:42:27 -0500

    add sdes.py for feynman_kac template

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='sdepy',
-    version='1.13',
+    version='1.14',
     packages=['sdepy'],
     url='https://github.com/shill1729/sdepy',
-    requires=["numpy", "sympy", "matplotlib"],
+    requires=["numpy", "sympy", "matplotlib", "scipy"],
     license='MIT',
     author='Sean Hill',
     author_email='52792611+shill1729@users.noreply.github.com',
